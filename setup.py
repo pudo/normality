270baf01ec4d6f84fc9c4ded9c5c04a532b96d57
Friedrich Lindenberg <friedrich@pudo.org>
2015-01-24 12:08:19 +0100

    Python package spec

---
+++ setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='normality',
+    version='0.1',
+    description="Micro-library to normalize text strings",
+    long_description="",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.3'
+    ],
+    keywords='text unicode normalization slugs',
+    author='Friedrich Lindenberg',
+    author_email='friedrich@pudo.org',
+    url='http://github.com/pudo/normality',
+    license='MIT',
+    packages=find_packages(exclude=['ez_setup', 'examples', 'test']),
+    namespace_packages=[],
+    include_package_data=False,
+    zip_safe=False,
+    install_requires=[
+        'six',
+        'python-slugify >= 0.0.6'
+    ],
+    tests_require=[],
+    test_suite='test',
+    entry_points={
+    }
+)
