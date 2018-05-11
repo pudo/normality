65c25b9a3c28aa49e2a705ddb69f7a0013cfab6d
Friedrich Lindenberg <friedrich@pudo.org>
2018-05-11 10:02:34 +0200

    Bump version: 0.5.12 → 0.6.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='normality',
-    version='0.5.12',
+    version='0.6.0',
     description="Micro-library to normalize text strings",
     long_description="",
     classifiers=[
