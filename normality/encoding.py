fa8ad8bf8dff79716a53c8ee555fc62ca7c50cbf
Friedrich Lindenberg <friedrich@pudo.org>
2018-05-14 12:29:40 +0200

    Disable cchardet

--- normality/encoding.py
+++ normality/encoding.py
@@ -1,10 +1,6 @@
 import io
-import six
 import codecs
-try:
-    import cchardet as chardet
-except ImportError:
-    import chardet
+import chardet
 
 DEFAULT_ENCODING = 'utf-8'
 
@@ -25,9 +21,11 @@ def normalize_encoding(encoding, default=DEFAULT_ENCODING):
 
 def normalize_result(result, default, threshold=0.2):
     """Interpret a chardet result."""
-    if (result is None or
-        result.get('confidence') is None or
-            result.get('confidence') < threshold):
+    if result is None:
+        return default
+    if result.get('confidence') is None:
+        return default
+    if result.get('confidence') < threshold:
         return default
     return normalize_encoding(result.get('encoding'),
                               default=default)
@@ -47,20 +45,20 @@ def guess_file_encoding(fh, default=DEFAULT_ENCODING):
     """Guess encoding from a file handle."""
     start = fh.tell()
     detector = chardet.UniversalDetector()
-    for idx in six.moves.range(1024):
-        data = fh.read(1024)
-        if not len(data):
+    while True:
+        data = fh.read(1024 * 10)
+        if not data:
+            detector.close()
             break
         detector.feed(data)
         if detector.done:
             break
 
-    detector.close()
     fh.seek(start)
     return normalize_result(detector.result, default=default)
 
 
 def guess_path_encoding(file_path, default=DEFAULT_ENCODING):
     """Wrapper to open that damn file for you, lazy bastard."""
-    with io.open(file_path, 'r') as fh:
+    with io.open(file_path, 'rb') as fh:
         return guess_file_encoding(fh, default=default)
