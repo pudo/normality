38d229c71353ff11f1359016268ffa0af14b542e
Friedrich Lindenberg <friedrich@pudo.org>
2022-03-24 21:51:36 +0100

    Bump version: 2.3.1 → 2.3.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md") as f:
 
 setup(
     name="normality",
-    version="2.3.1",
+    version="2.3.2",
     description="Micro-library to normalize text strings",
     long_description=long_description,
     long_description_content_type="text/markdown",
