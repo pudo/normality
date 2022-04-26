66a58fa9e05c6c426b4c7b9d781104d0a94e3e22
Dmitry Marakasov <amdmi3@amdmi3.ru>
2022-04-26 15:16:43 +0300

    Fix tests package name when excluding it

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ setup(
     url="http://github.com/pudo/normality",
     license="MIT",
     package_data={"banal": ["py.typed"]},
-    packages=find_packages(exclude=["ez_setup", "examples", "test"]),
+    packages=find_packages(exclude=["ez_setup", "examples", "tests"]),
     namespace_packages=[],
     include_package_data=True,
     zip_safe=False,
