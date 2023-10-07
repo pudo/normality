aa64968a0a4460f805da67f8880753917e667a38
Friedrich Lindenberg <friedrich@pudo.org>
2023-10-07 21:44:29 +0200

    handle unicode block ranges and map them out a bit

---
+++ tests/test_scripts.py
@@ -0,0 +1,17 @@
+from normality.scripts import ALPHABET, CYRILLIC, CJK
+from normality.scripts import char_tags, is_modern_alphabet
+
+
+def test_char_tags():
+    assert ALPHABET in char_tags("a")
+    assert CYRILLIC not in char_tags("a")
+    assert CYRILLIC in char_tags("д")
+    assert CJK in char_tags("近")
+    assert ALPHABET not in char_tags("近")
+
+
+def test_is_modern_alphabet():
+    assert not is_modern_alphabet(" 习近平")
+    assert is_modern_alphabet("Xí Jìnpíng")
+    assert is_modern_alphabet("Ротенберг Аркадий")
+    assert not is_modern_alphabet("တပ်မတော်(ကြည်")
